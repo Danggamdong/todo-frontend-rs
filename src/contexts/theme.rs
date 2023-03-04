444cadb199dba6f562276f88a4f1b30e96e58d84
misut <pkr5207@gmail.com>
2023-03-04 16:19:59 +0900

    feat: ui theme

---
+++ src/contexts/theme.rs
@@ -0,0 +1,94 @@
+use std::ops::Deref;
+
+use once_cell::sync::Lazy;
+use stylist::yew::*;
+use yew::prelude::*;
+
+pub struct Theme {
+    pub background_color: String,
+    pub font_color: String,
+    pub paper_color: String,
+}
+
+#[derive(Clone, PartialEq)]
+pub enum ThemeKind {
+    Dark,
+    Light,
+}
+
+impl ThemeKind {
+    pub fn current(&self) -> &Theme {
+        static DARK_THEME: Lazy<Theme> = Lazy::new(|| Theme {
+            font_color: "white".to_string(),
+            background_color: "black".to_string(),
+            paper_color: "rgb(50, 50, 50)".to_string(),
+        });
+
+        static LIGHT_THEME: Lazy<Theme> = Lazy::new(|| Theme {
+            font_color: "black".to_string(),
+            background_color: "rgb(237, 244, 255)".to_string(),
+            paper_color: "white".to_string(),
+        });
+
+        match self {
+            ThemeKind::Dark => &DARK_THEME,
+            ThemeKind::Light => &LIGHT_THEME,
+        }
+    }
+}
+
+#[derive(Clone)]
+pub struct ThemeContext {
+    inner: UseStateHandle<ThemeKind>
+}
+
+impl ThemeContext {
+    pub fn new(inner: UseStateHandle<ThemeKind>) -> Self {
+        Self { inner }
+    }
+
+    pub fn set(&self, kind: ThemeKind) {
+        self.inner.set(kind)
+    }
+
+    pub fn kind(&self) -> ThemeKind {
+        (*self.inner).clone()
+    }
+}
+
+impl Deref for ThemeContext {
+    type Target = Theme;
+
+    fn deref(&self) -> &Self::Target {
+        self.inner.current()
+    }
+}
+
+impl PartialEq for ThemeContext {
+    fn eq(&self, rhs: &Self) -> bool {
+        *self.inner == *rhs.inner
+    }
+}
+
+#[derive(PartialEq, Properties)]
+pub struct ThemeProviderProps {
+    pub children: Children,
+}
+
+#[styled_component]
+pub fn ThemeProvider(props: &ThemeProviderProps) -> Html {
+    let theme_kind = use_state(|| ThemeKind::Light);
+
+    let theme_ctx = ThemeContext::new(theme_kind);
+
+    html! {
+        <ContextProvider<ThemeContext> context={theme_ctx}>
+            {props.children.clone()}
+        </ContextProvider<ThemeContext>>
+    }
+}
+
+#[hook]
+pub fn use_theme() -> ThemeContext {
+    use_context::<ThemeContext>().unwrap()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 use chrono::prelude::*;
 use gloo_net::http::Request;
+use stylist::yew::*;
 use yew::prelude::*;
 
 mod components;
+mod contexts;
 mod state;
 
 use components::entry::Entry;
 use components::header_input::HeaderInput;
+use contexts::theme::{ThemeProvider, use_theme};
 use state::{State, Todo};
 
-#[function_component]
-fn App() -> Html {
+#[styled_component]
+fn Main() -> Html {
     let state = use_state(|| State { todos: vec![] });
     {
         let state = state.clone();
@@ -66,24 +69,82 @@ fn App() -> Html {
     };
 
     html! {
-        <div class="todo-wrapper">
-            <section class="todo-app">
-                <header class="header">
-                    <h1>{ "todos" }</h1>
-                    <HeaderInput {onadd} />
-                </header>
-                <section class="main">
-                    <ul class="todo-list">
-                        {for state.todos.iter().cloned().map(|todo| html! {<Entry {todo} onremove={onremove.clone()}/>})}
-                    </ul>
-                </section>
-                <footer class="footer">
-                </footer>
-            </section>
+        <div id="todo-main">
+            <div id="header">
+                <h1>{ "todos" }</h1>
+                <HeaderInput {onadd} />
+            </div>
+            <div id="main">
+                <ul id="todo-list">
+                    {for state.todos.iter().cloned().map(|todo| html! {<Entry {todo} onremove={onremove.clone()}/>})}
+                </ul>
+            </div>
+            <div id="footer">
+            </div>
         </div>
     }
 }
 
+#[styled_component]
+fn App() -> Html {
+    let theme = use_theme();
+
+    html! {
+        <>
+            <Global css={css!(
+                r#"
+                    html {
+                        font-family: sans-serif;
+
+                        padding: 0;
+                        margin: 0;
+
+                        display: flex;
+                        justify-content: center;
+                        align-items: center;
+                        min-height: 100vh;
+                        flex-direction: column;
+
+                        background-color: ${bg};
+                        color: ${ft_color};
+                    }
+                "#,
+                bg = theme.background_color.clone(),
+                ft_color = theme.font_color.clone(),
+            )} />
+            <header>
+            </header>
+            <section class={css!(
+                r#"
+                    box-shadow: 0 0 5px 1px rgba(0, 0, 0, 0.7);
+                    height: 500px;
+                    width: 500px;
+                    border-radius: 5px;
+                    display: flex;
+                    justify-content: space-around;
+                    align-items: center;
+                    padding: 15px;
+                    box-sizing: border-box;
+                    flex-direction: column;
+                    background-color: ${bg};
+                "#,
+                bg = theme.paper_color.clone()
+            )} id="todo-wrapper">
+                <Main />
+            </section>
+        </>
+    }
+}
+
+#[styled_component]
+pub fn Root() -> Html {
+    html! {
+        <ThemeProvider>
+            <App />
+        </ThemeProvider>
+    }
+}
+
 fn main() {
-    yew::Renderer::<App>::new().render();
+    yew::Renderer::<Root>::new().render();
 }
